09414861faf7b006a4e377033f5ff795db9ce427
Magnus Lilledahl <Magnus Lilledahl 29771060+NLOM-NTNU-PI@users.noreply.github.com>
2023-10-29 18:22:34 +0100

    ps09

---
+++ ps09/heat2d.f90
@@ -0,0 +1,15 @@
+! To solve 2D heat equation
+
+module declarations
+! Decleare the necessary variables.
+end module
+
+module procedures
+! Procedures
+end module
+
+
+program heat2d
+! external subroutines
+end program
+
