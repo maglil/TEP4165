532082d66fb36b62a98a83766b3be5c738247a2e
maglil <29771060+maglil@users.noreply.github.com>
2023-10-30 15:55:58 +0100

    ps09

--- ps09/heat2d.f90
+++ ps09/heat2d.f90
@@ -7,16 +7,23 @@ module declarations
 	private
 	double precision,public, parameter :: pi=3.141592653589793
 	double precision,public, allocatable, dimension(:) :: x, x_face, y, y_face ! x and y coordinates of nodes and cell faces. Left/south face same index as node. (should these be 2D?)
+	double precision,public :: dx,dy
 	double precision,public :: xl, yl ! extent of simulation domain
 	
 	double precision,public, allocatable, dimension(:,:) :: aW,aE,aP,aN,aS
 	double precision,public, allocatable, dimension(:,:) :: T,thermal	! Temperature, thermal conductivity
 	double precision,public, allocatable, dimension(:,:) :: Sp,Su	
 	
-	double precision,public :: rho, Tamb, radius, perim, thermal_const, h, q 
+	double precision,public :: rho, Tamb, perim, thermal_const, h, q 
+	double precision,public :: width, height
 	
 	double precision,public :: relax(1)
 	integer,public :: iter,last,npi,npj
+	double precision, public :: norm0,norm1,dnorm0, n ! norms
+	double precision, public :: eps ! convergence criterion
+	
+	
+	double precision, public :: norm
   
 end module
 
@@ -56,10 +63,44 @@ program heat2d
 	call init()
 	call grid()
 	
-	call bound() 
-	call Tcoeff()
+	allocate(aE(npi,npj),aW(npi,npj),aN(npi,npj), aS(npi,npj), aP(npi,npj))
 	
+	call bound() 
+	
+	call calc_norm()
+	norm0 = norm
+	
+	do iter=1,last
 
+		call Tcoeff()
+		call gauss_seidel(T,Su,1,npi,1,npj)
+		
+		if (iter==1) then
+			call calc_norm()
+			norm1 = norm
+			dnorm0 = norm1-norm0
+			write(*,*) norm1-norm0, 'first'
+		end if
+		
+		if(mod(iter,200)==0)then
+			call calc_norm()
+			norm0 = norm1
+			norm1 = norm
+			write(*,*) (norm1-norm0)/dnorm0
+			write (*,*) 'iteration no:',iter,'  Temperatur:',(T(int((npi)/2),int((npj)/2))+T(int((npi)/2+1),int((npj)/2+1)))/2
+			if ((norm1-norm0)/dnorm0 < eps) then
+				exit
+			end if
+		end if
+		
+		
+	end do
+	
+	if(mod(iter,200)==0)then
+      write (*,*) 'iteration no:',iter,'  Temperatur:',T(int(npi/2),int(npj/2))
+    end if
+	
+	call printout()
 end program
 
 ! external subroutines
@@ -90,8 +131,9 @@ subroutine init()
 !---Set physical quantities
 	Tamb=290
     thermal_const=50.
-    radius=0.01
-    perim=pi*2*radius
+    width = 0.5
+	height = 0.2
+    !perim=pi*2*radius
 	h=80. ! heat loss coefficient
 	q = 100E3
 	
@@ -105,6 +147,9 @@ subroutine init()
 	  end do
     end do
 	
+!---Set convergence criterion
+	eps = 1.0e-6
+	
 !---Set relaxation parameter
 end subroutine
 
@@ -115,7 +160,7 @@ subroutine grid()
     implicit none
 	
     integer :: i,j
-    double precision :: dx,dy
+    
 	
 	allocate(x(npi),x_face(npi),y(npj), y_face(npj))
 	
@@ -125,7 +170,7 @@ subroutine grid()
 	
 !---Size of cell
 	dx=xl/real(npi-2.)
-	dy=yl/real(npi-2.)
+	dy=yl/real(npj-2.)
 	
 !---Set node coordinates
 	x(1)=0.
@@ -133,13 +178,14 @@ subroutine grid()
     do i=3,npi-1
       x(i)=x(i-1)+dx
     end do
-    y(npj)=x(npj-1)+0.5*dy
+    x(npi)=x(npi-1)+0.5*dy
+	
 	y(1)=0.
     y(2)=0.5*dy
     do i=3,npi-1
       y(i)=y(i-1)+dy
     end do
-    y(npi)=y(npi-1)+0.5*dy
+    y(npj)=y(npj-1)+0.5*dy
 	
 !---Set face coordinates
 	x_face(1)=0.
@@ -180,7 +226,57 @@ subroutine Tcoeff()
 	
 	use declarations
     implicit none
-    integer :: i
-    double precision :: AREAw,AREAe,Dw,De
-
+    integer :: i, j
+    double precision :: areaw,areae,arean,areas,Dw,De,Ds,Dn
+	
+	! Cell face areas
+	! Assume length of slab is 1 m.
+	! Move inside loop if dy,dx variable
+	areaw = dy*1.
+	areae = areaw
+	areas = dx*1.
+	arean = areas
+	
+	! D = k*A/dx Diffusion conductance
+	
+	do i = 2,npi-1
+		do j = 2, npj-1
+		Dw = (thermal(i-1,j) + thermal(i,j))/2*(x(i) - x(i-1)) * areaw
+		De = (thermal(i+1,j) + thermal(i,j))/2*(x(i+1) - x(i)) * areae
+		Ds = (thermal(i,j-1) + thermal(i,j))/2*(y(j) - y(j-1)) * areas
+		Dn = (thermal(i,j+1) + thermal(i,j))/2*(y(j+1) - y(j)) * arean
+		
+		! Set source if present
+		
+		! Set coefficients
+		aW(i,j) = Dw		
+		aE(i,j) = De
+		aS(i,j) = Ds
+		aN(i,j) = Dn		
+		aP(i,j) = aW(i,j)+aE(i,j)+aS(i,j)+aN(i,j) + Sp(i,j)				
+		end do
+		
+	end do
 end subroutine
+
+subroutine calc_norm()
+
+	use declarations
+	implicit none
+	
+	!double precision, intent(in) :: T(npi,npj)
+	!double precision, intent(in) :: dx,dy
+	!integer, intent(in) :: npi, npj
+	integer :: i,j
+	
+	norm = 0;
+	
+	do i=2,npi-1
+		do j=2,npi-1
+			norm = norm + (T(i,j)**2)			
+		end do
+	end do	
+	norm = sqrt(dx*dy*norm)
+	return
+	
+end subroutine calc_norm
