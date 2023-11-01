d45c1f65fcd6d29255f729a6abfb07469dde84e3
Magnus Lilledahl <Magnus Lilledahl 29771060+NLOM-NTNU-PI@users.noreply.github.com>
2023-11-01 22:03:53 +0100

    started convdiff2d

---
+++ ps10/convdiff2d.f90
@@ -0,0 +1,15 @@
+
+
+program convdiff2d
+
+! set physical variables
+
+! define variables
+
+! initalize variables
+
+! Define equations system coefficients
+
+! Solve linear system
+write(*,*) 'Completed program'
+end program convdiff2d
